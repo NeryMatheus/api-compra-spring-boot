6f2e3c1850a1f12ee583f25d48830ff4dfc68733
Matheus Nery <matheusnery345@gmail.com>
2022-06-22 19:45:42 -0400

    Implementaçõs Swagger

--- src/main/java/com/compras/compras/controller/CompraController.java
+++ src/main/java/com/compras/compras/controller/CompraController.java
@@ -3,6 +3,7 @@ package com.compras.compras.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,36 +14,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.compras.compras.repository.CompraRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import com.compras.compras.model.Compra;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API Compras")
+@CrossOrigin(origins = "*") // Libera todos os domínios
 public class CompraController {
     
     @Autowired
     CompraRepository compraRepository;
 
     @GetMapping("/compras")
+    @ApiOperation(value = "Retorna uma lista de compras")
     public List<Compra> listAllCompras(){
         return compraRepository.findAll();
     }
 
-    @GetMapping("compra/{id}")
+    @GetMapping("/compra/{id}")
+    @ApiOperation(value = "Retorna uma compras po Id")
     public Compra listCompraById (@PathVariable(value = "id") long id){
         return compraRepository.findById(id);
     }
 
     @PostMapping("/compra")
+    @ApiOperation(value = "Este método salva uma compra")
     public Compra saveCompras(@RequestBody Compra compra){
         return compraRepository.save(compra);
     }
 
     @DeleteMapping("/compra")
+    @ApiOperation(value = "Este método deleta uma compra")
     public void deleteCompra (@RequestBody Compra compra) {
         compraRepository.delete(compra);
     }
 
     @PutMapping("/compra")
+    @ApiOperation(value = "Este método atualiza uma compra")
     public Compra putCompra (@RequestBody Compra compra){
         return compraRepository.save(compra);
     }
